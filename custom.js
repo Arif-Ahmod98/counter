94dafc77565468bfa4b858249fd655c2909925c6
arif ahmod <arifahmod9c@gmail.com>
2022-01-26 22:25:39 +0600

    made counter with vanilla javascript

---
+++ custom.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let counter = document.querySelector(".counter");
+let buttons = document.querySelectorAll("button");
+let count = 0;
+
+buttons.forEach( function(button) {
+    button.addEventListener("click", function (e) {
+
+        let text = e.currentTarget.classList;
+        if (text.contains('decrease')){
+            count --;
+            counter.textContent = count;
+        }
+        else if ( text.contains('increase') ) {
+
+            count ++;
+            counter.textContent = count;
+
+        }
+        else {
+            count = 0;
+            counter.textContent = count;
+
+        };
+
+        if (count < 0) {
+            counter.style.color = "red";
+          
+        }
+        else if ( count > 0) {
+            counter.style.color = "green";
+        }
+        else {
+            counter.style.color = "black";
+        }
+
+    });
+});
